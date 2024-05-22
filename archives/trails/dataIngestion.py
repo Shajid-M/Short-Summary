c6688bb7a8fa9963beb172e3baf5eed38bd11d8e
Shajid-M <shajid24mughal@gmail.com>
2024-05-23 00:59:53 +0530

    regular tasks in the setup procedure

---
+++ archives/trails/dataIngestion.py
@@ -0,0 +1,13 @@
+from shortSummary.constants import *
+from shortSummary.utils.common import read_yaml, create_directories
+
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])
